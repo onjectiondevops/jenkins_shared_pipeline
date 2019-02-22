3d3403f0904a9926aaf9e05ba9f11d297e21e577
Amit Dahiya <Amit.dahiya@onjection.com>
2019-02-22 16:09:52 +0530

    Add ci

--- vars/evenOrOdd.groovy
+++ vars/evenOrOdd.groovy
@@ -1,24 +1,16 @@
 def call(int buildNumber) {
   if (buildNumber % 2 == 0) {
     pipeline {
-      stages {
         stage('Even Stage') {
-          steps {
             echo "The build number is even"
 	    sh "ls -lhtr /"
           }
-        }
-      }
     }
   } else {
     pipeline {
-      stages {
         stage('Odd Stage') {
-          steps {
             echo "The build number is odd"
 	    sh "ls -lhtr /"
-          }
-        }
       }
     }
   }
