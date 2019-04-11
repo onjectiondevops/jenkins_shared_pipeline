a3fd10fdb177fcf6b26decfc89c1ea61b810668a
Amit Dahiya <Amit.dahiya@onjection.com>
2019-04-11 13:45:37 +0530

    add ci/cd codes

--- src/io/abc/pipeline.groovy
+++ src/io/abc/pipeline.groovy
@@ -24,71 +24,87 @@ def checkOutScmAuto(STAGENAME){
    }
 }
 
-def MvnBuild(name) {
-  def scannerHome = tool 'mvn'
-  sh "${scannerHome}/bin/mvn ${name}"
+def MvnBuild(STAGENAME, name) {
+   stage(STAGENAME){
+      def scannerHome = tool 'mvn'
+      sh "${scannerHome}/bin/mvn ${name}"
+   }
 }
 
-def sonarScan(server, projectname, projectkey) {
-  	def SonarQubescannerHome = tool 'sonarqube-scanner'
-	  sh "${SonarQubescannerHome}/bin/sonar-scanner -Dsonar.host.url=${server} -Dsonar.projectKey=${projectname} -Dsonar.projectName=${projectkey} -Dsonar.sources=. -Dsonar.projectVersion=1.0"
-
+def sonarScan(STAGENAME, server, projectname, projectkey) {
+   stage(STAGENAME){
+  	  def SonarQubescannerHome = tool 'sonarqube-scanner'
+	    sh "${SonarQubescannerHome}/bin/sonar-scanner -Dsonar.host.url=${server} -Dsonar.projectKey=${projectname} -Dsonar.projectName=${projectkey} -Dsonar.sources=. -Dsonar.projectVersion=1.0"
+   }
 }
 
-def dockerBuild(imagename) {
-	sh "docker build -t ${imagename} ."
-}
-
-def dockerLoginandPush(HUBCREDENTIALID, REGISTRY, IMAGENAME){
-    withDockerRegistry(credentialsId: HUBCREDENTIALID, url: "${REGISTRY}") {
-        sh "docker push ${IMAGENAME}"
-    }
-}
-
-def onlyMasterSteps(stepsToRun) {
-  if (gitBranch == 'master') {
-        echo "Current branch name is: ${gitBranch}"
-        stepsToRun
-    }
-}
-
-def stopOtherThanMaster() {
-  if (gitBranch != 'master') {
-        error "Pipeline is not executing from Master branch. Stopping the pipeline."
-        return
-    }
-}
-
-def remoteDockerDeploy(IP, USERNAME, ssh_credentials_id, COMMAND){
-    withCredentials([string(credentialsId: ssh_credentials_id, variable: 'PASS')]) {
-      def remote = [:]
-      remote.name = "test"
-      remote.host = "${IP}"
-      remote.user = "${USERNAME}"
-      remote.password = PASS
-      remote.allowAnyHosts = true
-      sshCommand remote: remote, command: "${COMMAND}"
-    }
-}
-
-def kubernetesDeployment(CLUSTERNAME, APIENDPOINT, KUBERNETESCREDENTIALID, KUBERNETESCOMMANDS){
- withKubeConfig(caCertificate: '', clusterName: "${CLUSTERNAME}", contextName: '', credentialsId: "${KUBERNETESCREDENTIALID}", namespace: '', serverUrl: "${APIENDPOINT}") {
-    sh 'rm -rf ~/.helm && helm init --client-only'
-    sh """ ${KUBERNETESCOMMANDS} """
+def dockerBuild(STAGENAME, imagename) {
+  stage(STAGENAME){
+	    sh "docker build -t ${imagename} ."
   }
 }
 
-//def blackDuckScan(BLACKDUCKSEVERURL, USERNAME, ) {
-//hub-detect --blackduck.hub.url=BLACKDUCKSEVERURL --blackduck.hub.username=username --blackduck.hub.password=******* --blackduck.hub.trust.cert=true
-//}
-
-def setEnvironment(DOCKERIMAGENAME, BUILDSTEPSTORUN){
-    docker.image(DOCKERIMAGENAME).inside {
-        sh """ ${BUILDSTEPSTORUN} """
-    }
-
+def dockerLoginandPush(STAGENAME, HUBCREDENTIALID, REGISTRY, IMAGENAME){
+  stage(STAGENAME){
+      withDockerRegistry(credentialsId: HUBCREDENTIALID, url: "${REGISTRY}") {
+          sh "docker push ${IMAGENAME}"
+      }
+  }
 }
 
+def onlyMasterSteps(STAGENAME, stepsToRun) {
+   stage(STAGENAME){
+      if (gitBranch == 'master') {
+            echo "Current branch name is: ${gitBranch}"
+            stepsToRun
+        }
+   }
+}
 
+def stopOtherThanMaster(STAGENAME) {
+  stage(STAGENAME){
+      if (gitBranch != 'master') {
+            error "Pipeline is not executing from Master branch. Stopping the pipeline."
+            return
+        }
+  }
+}
+
+def remoteDockerDeploy(STAGENAME, IP, USERNAME, ssh_credentials_id, COMMAND){
+   stage(STAGENAME){
+        withCredentials([string(credentialsId: ssh_credentials_id, variable: 'PASS')]) {
+          def remote = [:]
+          remote.name = "test"
+          remote.host = "${IP}"
+          remote.user = "${USERNAME}"
+          remote.password = PASS
+          remote.allowAnyHosts = true
+          sshCommand remote: remote, command: "${COMMAND}"
+        }
+    }
+}
+
+def kubernetesDeployment(STAGENAME, CLUSTERNAME, APIENDPOINT, KUBERNETESCREDENTIALID, KUBERNETESCOMMANDS){
+  stage(STAGENAME){
+     withKubeConfig(caCertificate: '', clusterName: "${CLUSTERNAME}", contextName: '', credentialsId: "${KUBERNETESCREDENTIALID}", namespace: '', serverUrl: "${APIENDPOINT}") {
+        sh 'rm -rf ~/.helm && helm init --client-only'
+        sh """ ${KUBERNETESCOMMANDS} """
+      }
+  }
+}
+
+//def blackDuckScan(STAGENAME, BLACKDUCKSEVERURL, USERNAME, ) {
+//   stage(STAGENAME){
+//      hub-detect --blackduck.hub.url=BLACKDUCKSEVERURL --blackduck.hub.username=username --blackduck.hub.password=******* --blackduck.hub.trust.cert=true
+//   }
+//}
+
+def setEnvironmentSTAGENAME, (DOCKERIMAGENAME, BUILDSTEPSTORUN){
+  stage(STAGENAME){
+      docker.image(DOCKERIMAGENAME).inside {
+          sh """ ${BUILDSTEPSTORUN} """
+      }
+  }
+}
 
 return this
