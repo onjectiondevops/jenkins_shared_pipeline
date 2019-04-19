87e14f4ef23ea5be47747a23a25ae9cb377f7a44
Amit Dahiya <Amit.dahiya@onjection.com>
2019-04-19 18:12:49 +0530

    add codes

--- src/io/abc/pipeline.groovy
+++ src/io/abc/pipeline.groovy
@@ -146,7 +146,7 @@ def junitPublish(STAGENAME, PATHOFARTIFACT){
 def seleniumTest(STAGENAME, SELENIUM_NODE_IMAGE, RUN_TIME_ARGUMENTS, COMMANDS_TO_RUN){
 stage(STAGENAME){
       docker.image(SELENIUM_NODE_IMAGE).withRun(RUN_TIME_ARGUMENTS) { c ->
-          sh "COMMANDS_TO_RUN"
+          sh """ ${COMMANDS_TO_RUN} """
      }
    }
 }
