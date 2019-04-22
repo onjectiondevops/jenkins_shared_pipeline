e55da80c53c6d923c00d2e5e262d6389c4e1ab29
Amit Dahiya <Amit.dahiya@onjection.com>
2019-04-22 13:50:37 +0530

    add codes

--- src/io/abc/pipeline.groovy
+++ src/io/abc/pipeline.groovy
@@ -30,7 +30,7 @@ def CheckOutScmAuto(STAGE_NAME){
 def SonarScan(STAGE_NAME, SERVER, PROJECT_NAME, PROJECT_KEY, CODE_DIR) {
    stage(STAGE_NAME){
   	  def SonarQubescannerHome = tool 'sonarqube-scanner'
-	    sh "cd ${CODE_DIR} && ${SonarQubescannerHome}/bin/sonar-scanner -Dsonar.host.url=${SERVER} -Dsonar.projectKey=${PROJECT_NAME} -Dsonar.projectName=${PROJECT_KEY} -Dsonar.sourceEncoding=UTF-8 -Dsonar.webhooks.global.1.url=${SERVER}  -Dsonar.sources=. -Dsonar.projectVersion=1.0"
+	    sh "cd ${CODE_DIR} && ${SonarQubescannerHome}/bin/sonar-scanner -Dsonar.host.url=${SERVER} -Dsonar.projectKey=${PROJECT_NAME} -Dsonar.java.binaries=bin -Dsonar.projectName=${PROJECT_KEY} -Dsonar.sourceEncoding=UTF-8 -Dsonar.webhooks.global.1.url=${SERVER}  -Dsonar.sources=. -Dsonar.projectVersion=1.0"
    }
 }
 
