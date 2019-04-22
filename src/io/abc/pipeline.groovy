2219ac4a2643243d5d5bdd424e42eb74d8d46974
Amit Dahiya <Amit.dahiya@onjection.com>
2019-04-22 11:38:31 +0530

    add codes

--- src/io/abc/pipeline.groovy
+++ src/io/abc/pipeline.groovy
@@ -146,7 +146,8 @@ def KubernetesDeployment(STAGE_NAME, CLUSTER_NAME, API_END_POINT, KUBERNETES_CRE
 
 def PipelineLogsArtifact(STAGE_NAME){
     stage(STAGE_NAME){
-      archiveArtifacts artifacts: "${JENKINS_HOME}/jobs/${JOB_NAME}/builds/${BUILD_NUMBER}/log", fingerprint: true
+      sh "cat ${JENKINS_HOME}/jobs/${JOB_NAME}/builds/${BUILD_NUMBER}/log >> pipeline_logs.txt"
+      archiveArtifacts artifacts: "pipeline_logs.txt", fingerprint: true
     }
 }
 
